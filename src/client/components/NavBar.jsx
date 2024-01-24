55c2bf02a0bdf38f8660cc5f8e1ff4939a59b711
Collin Bullock <cbullock@schooloftock.com>
2024-01-24 09:46:52 -0600

    created pages for the navbar links menu about and contact.  They are currently blank

--- src/client/components/NavBar.jsx
+++ src/client/components/NavBar.jsx
@@ -5,10 +5,12 @@ import Navbar from 'react-bootstrap/Navbar';
 export default function NavBar() {
   return (
     <>
+    {/* Chose basic dark tempelate.  Once we settle on a color scheme and all that jazz we can greatly modify the basic a$$ bootstrap stuff */}
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Collin And Tony's House Of Sushi and Pizza</Navbar.Brand>
           <Nav className="me-auto">
+            {/* Starting with these three basic pages, we can add more.  Maybe like a page for private parties or something?  An events calendar we can try and import from google calendars or something?  I dunno, just ideas! */}
             <Nav.Link href="/menu">Menu</Nav.Link>
             <Nav.Link href="/about">Our Story</Nav.Link>
             <Nav.Link href="/contact">Contact Us</Nav.Link>
