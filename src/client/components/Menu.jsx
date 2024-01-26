7727aee71566544d2567778e8de3bad3b7f07cac
Collin Bullock <cbullock@schooloftock.com>
2024-01-26 12:14:14 -0600

    added an apps array, mapped it out inside the menu component

--- src/client/components/Menu.jsx
+++ src/client/components/Menu.jsx
@@ -1,7 +1,7 @@
 // we'll have to input various bootstrap components as we build this out, but this is just a placeholder.
 // I'm thinking, in our data file, we could have sections for like drinks, apps, etc.  Then we just fetch and map out each section into individual cards and such
 
-import { drinksArray } from "./MenuArray"
+import { drinksArray, appsArray } from "./MenuArray"
 
 export default function Menu() {
   return (
@@ -13,6 +13,15 @@ export default function Menu() {
         <div key={index}>
           {/* once we get into styling, we'll have the image and maybe the title on the front of the card and then when the user clicks on said card it will flip over to display the price and description. */}
           {/* Image is super large on the page, we'll style it within the card */}
+          <img src={item.image} alt="A delicious beverage" />
+          <p>{item.title}</p>
+          <p>Price: {item.price}</p>
+          <p>{item.description}</p>
+        </div>
+      ))}
+      <h2>Appetizers</h2>
+      {appsArray.map((item, index) => (
+        <div key={index}>
           <img src={item.image} alt="A delicious beverage" />
           <p>{item.title}</p>
           <p>Price: {item.price}</p>
--- src/client/components/MenuArray.js
+++ src/client/components/MenuArray.js
@@ -8,9 +8,13 @@ const menuArray = [
     {"item": "4", "title": "House Red", "image": "https://www.thespruceeats.com/thmb/N9TEoqtQz-R9zkjMXM8I530sj30=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/red-wine-is-poured-into-a-glass-from-a-bottle--light-background--1153158143-98320451802c485cb6d7b5437c7fd60a.jpg", "price": "19.99 per bottole", "description": "Our house red combine a fruity oak flavor with a daring, almost romantic, tinger of aged elder berries personally stomped on by Italian Monks who have never met a woman and absolutely do not plan to." },
     {"item": "5", "title": "Assorted Fountain Drinks", "image": "https://southpark.garlicknotpizza.com/wp-content/uploads/sites/15/2020/08/grid-fountain-drinks2.png", "price": "4.99", "description": "We serve Coca Cola products here.  We do not respect Pepsi and we never will.  It's a degenerate soda line.  If you ask for Pepsi we will ask you politely, but firmly, to leave and to never come back."},
     {"item": "6", "title": "espresso", "image": "https://www.tasteofhome.com/wp-content/uploads/2023/03/TOH-espresso-GettyImages-1291298315-JVcrop.jpg", "price": "3.99", "description": "Our robust and bold espresso is made from beans harvested, individually, by the hands of small sicialian children who were paid in rice.  Also, we don't serve tea so stop asking.  We will NEVER serve tea."}
+  ],
+  [
+    {"item": "1", "title": "Edamame", "image": "https://i.ytimg.com/vi/_jNvqD9J53M/maxresdefault.jpg", "price": "7.99", "description": "Edamame are young soybeans harvested before they ripen or harden. Available shelled, in the pod, fresh, or frozen, they are a popular, plant-based food that may be good for a person's health. Edamame beans are naturally gluten-free and low in calories. They contain no cholesterol and provide protein, iron, and calcium."}
   ]
 ]
 
 const drinksArray = menuArray[0];
+const appsArray = menuArray[1];
 
-export { menuArray, drinksArray };
+export { menuArray, drinksArray, appsArray };
