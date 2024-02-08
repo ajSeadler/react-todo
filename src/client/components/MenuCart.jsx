cce927093eda19d8ad195d8b42d469e48fbee783
ajSeadler <aj6287@gmail.com>
2024-02-08 08:06:45 -0600

    added togo section

--- src/client/components/MenuCart.jsx
+++ src/client/components/MenuCart.jsx
@@ -36,7 +36,7 @@ export default function MenuCart() {
     setTimeout(() => {
       setShowSuccessAlert(false);
       setShowAgeConfirmation(false);
-    }, 3000); // Hide after 3 seconds
+    }, 3000); // remove after 3 seconds
   };
 
   const openDescriptionModal = (item) => {
@@ -66,9 +66,24 @@ export default function MenuCart() {
           position: 'fixed',
           top: '2%',
           left: 'calc(100% - 50px)',
-          zIndex: 9999, // Ensure it stays above other content
+          zIndex: 9999, 
         }}
       />
+      <Alert
+          variant="success"
+          show={showSuccessAlert}
+          style={{
+            position: 'fixed',
+            width:'80%',
+            margin:'5px',
+            top: '2%',
+            zIndex: 9998, 
+          }}
+          onClose={() => setShowSuccessAlert(false)}
+          dismissible
+        >
+          Item has been added to your cart. {showAgeConfirmation && "Are you 21 or over, punk?"}
+        </Alert>
       <Modal show={showCartModal} onHide={closeCartModal} style={{color:'#333'}}>
         <Modal.Header closeButton>
           <Modal.Title>Shopping Cart</Modal.Title>
@@ -89,15 +104,7 @@ export default function MenuCart() {
           </Button>
         </Modal.Footer>
       </Modal>
-      {/* Success Alert */}
-      <Alert
-        variant="success"
-        show={showSuccessAlert}
-        onClose={() => setShowSuccessAlert(false)}
-        dismissible
-      >
-        Item has been added to your cart. {showAgeConfirmation && "Are you 21 or over, punk?"}
-      </Alert>
+      
       <h1 className="mt-4" style={{ textDecoration: 'underline' }}>Menu</h1>
 
       {/* Drinks Section */}
@@ -118,10 +125,10 @@ export default function MenuCart() {
                     ? item.price.join(", ")
                     : item.price}
                 </Card.Text>
-                <Button variant="primary" onClick={() => addToCart(item)}>
+                <Button style={{backgroundColor:'#333'}} onClick={() => addToCart(item)}>
                   <FaShoppingCart /> Add to Cart
                 </Button>
-                <Button variant="link" onClick={() => openDescriptionModal(item)}>
+                <Button variant="link" style={{color:'#333'}} onClick={() => openDescriptionModal(item)}>
                   Show Description
                 </Button>
               </Card.Body>
@@ -148,10 +155,10 @@ export default function MenuCart() {
                     ? item.price.join(", ")
                     : item.price}
                 </Card.Text>
-                <Button variant="primary" onClick={() => addToCart(item)}>
+                <Button style={{backgroundColor:'#333'}} onClick={() => addToCart(item)}>
                   <FaShoppingCart /> Add to Cart
                 </Button>
-                <Button variant="link" onClick={() => openDescriptionModal(item)}>
+                <Button variant="link" style={{color:'#333'}} onClick={() => openDescriptionModal(item)}>
                   Show Description
                 </Button>
               </Card.Body>
@@ -160,7 +167,157 @@ export default function MenuCart() {
         ))}
       </Row>
 
-      {/* Modal for showing description */}
+                    {/* Soupds & Salads */}
+      <Row>
+        <h2 className="mt-4" style={{ color: '#fff' }}>Soups & Salad</h2>
+        {soupSaladArray.map((item, index) => (
+          <Col md={4} key={item.item} className="mb-4">
+            <Card>
+              <Card.Img
+                variant="top"
+                src={item.image}
+                style={{ maxHeight: "200px" }}
+              />
+              <Card.Body>
+                <Card.Title>{item.title}</Card.Title>
+                <Card.Text>
+                  {Array.isArray(item.price)
+                    ? item.price.join(", ")
+                    : item.price}
+                </Card.Text>
+                <Button style={{backgroundColor:'#333'}} onClick={() => addToCart(item)}>
+                  <FaShoppingCart /> Add to Cart
+                </Button>
+                <Button variant="link" style={{color:'#333'}} onClick={() => openDescriptionModal(item)}>
+                  Show Description
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+
+      <Row>
+        <h2 className="mt-4" style={{ color: '#fff' }}>Sushi Rolls</h2>
+        {rollsArray.map((item, index) => (
+          <Col md={4} key={item.item} className="mb-4">
+            <Card>
+              <Card.Img
+                variant="top"
+                src={item.image}
+                style={{ maxHeight: "200px" }}
+              />
+              <Card.Body>
+                <Card.Title>{item.title}</Card.Title>
+                <Card.Text>
+                  {Array.isArray(item.price)
+                    ? item.price.join(", ")
+                    : item.price}
+                </Card.Text>
+                <Button style={{backgroundColor:'#333'}} onClick={() => addToCart(item)}>
+                  <FaShoppingCart /> Add to Cart
+                </Button>
+                <Button variant="link" style={{color:'#333'}} onClick={() => openDescriptionModal(item)}>
+                  Show Description
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+
+      {/* Nigiri */}
+      <Row>
+        <h2 className="mt-4" style={{ color: '#fff' }}>Nigiri</h2>
+        {nigiriArray.map((item, index) => (
+          <Col md={4} key={item.item} className="mb-4">
+            <Card>
+              <Card.Img
+                variant="top"
+                src={item.image}
+                style={{ maxHeight: "200px" }}
+              />
+              <Card.Body>
+                <Card.Title>{item.title}</Card.Title>
+                <Card.Text>
+                  {Array.isArray(item.price)
+                    ? item.price.join(", ")
+                    : item.price}
+                </Card.Text>
+                <Button style={{backgroundColor:'#333'}} onClick={() => addToCart(item)}>
+                  <FaShoppingCart /> Add to Cart
+                </Button>
+                <Button variant="link" style={{color:'#333'}} onClick={() => openDescriptionModal(item)}>
+                  Show Description
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+
+
+                  {/* Calzones */}
+      <Row>
+        <h2 className="mt-4" style={{ color: '#fff' }}>Calzones</h2>
+        {calzoneArray.map((item, index) => (
+          <Col md={4} key={item.item} className="mb-4">
+            <Card>
+              <Card.Img
+                variant="top"
+                src={item.image}
+                style={{ maxHeight: "200px" }}
+              />
+              <Card.Body>
+                <Card.Title>{item.title}</Card.Title>
+                <Card.Text>
+                  {Array.isArray(item.price)
+                    ? item.price.join(", ")
+                    : item.price}
+                </Card.Text>
+                <Button style={{backgroundColor:'#333'}} onClick={() => addToCart(item)}>
+                  <FaShoppingCart /> Add to Cart
+                </Button>
+                <Button variant="link" style={{color:'#333'}} onClick={() => openDescriptionModal(item)}>
+                  Show Description
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+
+                    {/* pizzas */}
+      <Row>
+        <h2 className="mt-4" style={{ color: '#fff' }}>Pizza</h2>
+        {pizzaArray.map((item, index) => (
+          <Col md={4} key={item.item} className="mb-4">
+            <Card>
+              <Card.Img
+                variant="top"
+                src={item.image}
+                style={{ maxHeight: "200px" }}
+              />
+              <Card.Body>
+                <Card.Title>{item.title}</Card.Title>
+                <Card.Text>
+                  {Array.isArray(item.price)
+                    ? item.price.join(", ")
+                    : item.price}
+                </Card.Text>
+                <Button style={{backgroundColor:'#333'}} onClick={() => addToCart(item)}>
+                  <FaShoppingCart /> Add to Cart
+                </Button>
+                <Button variant="link" style={{color:'#333'}} onClick={() => openDescriptionModal(item)}>
+                  Show Description
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+
+      {/* Modal for showing menu item description */}
       <Modal show={selectedItem !== null} onHide={closeDescriptionModal} style={{ color: '#333' }}>
         <Modal.Header closeButton>
           <Modal.Title>{selectedItem && selectedItem.title}</Modal.Title>
