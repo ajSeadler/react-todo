5ba76c91020044b5ab2ed7c2da7d2445bd12853f
“CollinABullock” <collin@collinbullock.com>
2024-02-10 10:31:26 -0600

    continuing to build the card flip test

--- src/client/components/cardFlipTest.jsx
+++ src/client/components/cardFlipTest.jsx
@@ -1,1 +1,9 @@
 import React, {useState} from "react";
+import ReactCardFlip from "react-card-flip";
+import { drinksArray, appsArray, soupSaladArray, rollsArray, nigiriArray, calzoneArray, pizzaArray } from "./MenuArray";
+import { Navbar, Nav, Form, FormControl } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function cardFlipTest() {
+  const [flip, setFlip] = useState(false)
+}
